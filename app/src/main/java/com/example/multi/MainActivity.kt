912a1b622bed75107bc92ebb4b6a731dd488511a
Samuel <samuelpinop1999@gmail.com>
2025-06-12 13:36:33 -0400

    Better commented code!

--- app/src/main/java/com/example/multi/MainActivity.kt
+++ app/src/main/java/com/example/multi/MainActivity.kt
@@ -11,6 +11,14 @@ import androidx.compose.material.Scaffold
 import androidx.compose.ui.Modifier
 import com.example.multi.ui.theme.MultiTheme
 
+// Work with all 3 different Calendar API's
+/*
+   Create pop up message similar to Make Toast but instead of an ugly Make Toast a beautiful pop up.
+   It will happen whenever a New Event is added or a New Weekly activity is added.
+   It will say New Event added or New Weekly Activity added.
+*/
+// Fix dropdown issue in edit!
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
