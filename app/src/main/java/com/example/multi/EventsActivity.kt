6d7f8b66436321fc90e1558be040f22912c08a40
Samuel <samuelpinop1999@gmail.com>
2025-07-26 18:24:13 -0400

    Import added!

--- app/src/main/java/com/example/multi/EventsActivity.kt
+++ app/src/main/java/com/example/multi/EventsActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.CalendarMonth
 import androidx.compose.material.icons.filled.Event
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
