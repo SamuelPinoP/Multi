d5521f5f6e0b46702eb8ccd74f50fea6d32b9dde
Samuel <samuelpinop1999@gmail.com>
2025-08-21 00:19:34 -0400

    Imports!

--- app/src/main/java/com/example/multi/EventsActivity.kt
+++ app/src/main/java/com/example/multi/EventsActivity.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.CalendarMonth
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenuItem
