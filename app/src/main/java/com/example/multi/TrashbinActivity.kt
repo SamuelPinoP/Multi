d31433eca8c8acf7aadee4fe757e249303b9de5a
Samuel <samuelpinop1999@gmail.com>
2025-09-07 15:02:02 -0400

    Imported two important features!

--- app/src/main/java/com/example/multi/TrashbinActivity.kt
+++ app/src/main/java/com/example/multi/TrashbinActivity.kt
@@ -7,10 +7,12 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
